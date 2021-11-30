176b4134791ae2a3f24d556b03f1200f726efc67
Eduard Wayland <eduproard@gmail.com>
2021-11-30 22:05:58 +0100

    Documented IncomingMessageConsumer interface

--- common/src/gg/mooncraft/minecraft/bedwars/common/messaging/IncomingMessageConsumer.java
+++ common/src/gg/mooncraft/minecraft/bedwars/common/messaging/IncomingMessageConsumer.java
@@ -3,10 +3,16 @@ package gg.mooncraft.minecraft.bedwars.common.messaging;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A simply functional interface called by {@link RedisSubscription} with the received message
+ */
 @ApiStatus.NonExtendable
+@FunctionalInterface
 public interface IncomingMessageConsumer {
 
-    boolean consumeIncomingMessage(@NotNull Message message);
-
+    /**
+     * @param jsonMessage the json message to consume
+     * @return true if the message has been consumed else returns false
+     */
     boolean consumeIncomingMessageAsString(@NotNull String jsonMessage);
 }
