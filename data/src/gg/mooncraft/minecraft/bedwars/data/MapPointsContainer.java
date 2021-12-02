7b8e5f5704ced79ee83713175e9cfe94abc028c7
Eduard Wayland <eduproard@gmail.com>
2021-12-02 16:05:32 +0100

    Separated points per-team

--- data/src/gg/mooncraft/minecraft/bedwars/data/MapPointsContainer.java
+++ data/src/gg/mooncraft/minecraft/bedwars/data/MapPointsContainer.java
@@ -3,8 +3,10 @@ package gg.mooncraft.minecraft.bedwars.data;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.UnmodifiableView;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public final class MapPointsContainer {
 
@@ -12,12 +14,14 @@ public final class MapPointsContainer {
     Fields
      */
     private final @NotNull List<MapPoint> pointList;
+    private final @NotNull Map<GameTeam, List<MapPoint>> teamPointMap;
 
     /*
     Constructor
      */
     public MapPointsContainer() {
         this.pointList = new LinkedList<>();
+        this.teamPointMap = new HashMap<>();
     }
 
     /*
