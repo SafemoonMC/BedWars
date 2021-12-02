d861c4d8be88859926e69425699f0a8459675acd
Eduard Wayland <eduproard@gmail.com>
2021-12-02 01:43:51 +0100

    Initial BedWarsUser model

---
+++ data/src/gg/mooncraft/minecraft/bedwars/data/BedWarsUser.java
@@ -0,0 +1,38 @@
+package gg.mooncraft.minecraft.bedwars.data;
+
+import me.eduardwayland.mooncraft.waylander.database.entities.EntityParent;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigInteger;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+public final class BedWarsUser implements EntityParent<BedWarsUser> {
+
+    /*
+    Fields
+     */
+    private final @NotNull UUID uniqueId;
+    private @NotNull BigInteger coins;
+    private @NotNull BigInteger level;
+    private @NotNull BigInteger experience;
+
+    /*
+    Constructor
+     */
+    public BedWarsUser(@NotNull UUID uniqueId) {
+        this.uniqueId = uniqueId;
+        this.coins = BigInteger.ZERO;
+        this.level = BigInteger.ZERO;
+        this.experience = BigInteger.ZERO;
+    }
+
+    /*
+    Override Methods
+     */
+    @Override
+    public @NotNull CompletableFuture<BedWarsUser> withChildren() {
+        return CompletableFuture.completedFuture(this);
+    }
+}
