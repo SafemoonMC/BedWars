dd96258e857620b2ba200f5fd3e1ceb01bf9d810
Eduard Wayland <eduproard@gmail.com>
2021-12-02 16:00:51 +0100

    Added GameTeam with ChatColor support

---
+++ data/src/gg/mooncraft/minecraft/bedwars/data/GameTeam.java
@@ -0,0 +1,33 @@
+package gg.mooncraft.minecraft.bedwars.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import net.md_5.bungee.api.ChatColor;
+
+@Getter
+@AllArgsConstructor
+public enum GameTeam {
+
+    BLACK(ChatColor.BLACK),
+    DARK_BLUE(ChatColor.DARK_BLUE),
+    DARK_GREEN(ChatColor.DARK_GREEN),
+    DARK_AQUA(ChatColor.DARK_AQUA),
+    DARK_RED(ChatColor.DARK_RED),
+    DARK_PURPLE(ChatColor.DARK_PURPLE),
+    GOLD(ChatColor.GOLD),
+    GRAY(ChatColor.GRAY),
+    DARK_GRAY(ChatColor.DARK_GRAY),
+    BLUE(ChatColor.BLUE),
+    GREEN(ChatColor.GREEN),
+    AQUA(ChatColor.AQUA),
+    RED(ChatColor.RED),
+    LIGHT_PURPLE(ChatColor.LIGHT_PURPLE),
+    YELLOW(ChatColor.YELLOW),
+    WHITE(ChatColor.WHITE);
+
+    /*
+    Fields
+     */
+    private final ChatColor chatColor;
+}
