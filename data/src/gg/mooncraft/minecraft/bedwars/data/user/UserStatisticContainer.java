12728320892bf11f99b2d8ff4d5ac7434f3387df
Eduard Wayland <eduproard@gmail.com>
2021-12-04 14:01:04 +0100

    Added getters for user statistic container

--- data/src/gg/mooncraft/minecraft/bedwars/data/user/UserStatisticContainer.java
+++ data/src/gg/mooncraft/minecraft/bedwars/data/user/UserStatisticContainer.java
@@ -7,9 +7,13 @@ import me.eduardwayland.mooncraft.waylander.database.entities.EntityParent;
 
 import org.jetbrains.annotations.NotNull;
 
+import gg.mooncraft.minecraft.bedwars.data.GameMode;
+import gg.mooncraft.minecraft.bedwars.data.UserStatisticDAO;
 import gg.mooncraft.minecraft.bedwars.data.user.stats.GameStatistic;
 import gg.mooncraft.minecraft.bedwars.data.user.stats.OverallStatistic;
+import gg.mooncraft.minecraft.bedwars.data.user.stats.StatisticTypes;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -34,10 +38,35 @@ public final class UserStatisticContainer implements EntityChild<BedWarsUser>, E
     }
 
     /*
+    Methods
+     */
+    public @NotNull BigInteger getGameStatistic(@NotNull GameMode gameMode, @NotNull StatisticTypes.GAME type) {
+        return this.gameStatisticList
+                .stream()
+                .filter(gameStatistic -> gameStatistic.getGameMode() == gameMode && gameStatistic.getType() == type)
+                .findFirst()
+                .map(GameStatistic::getAmount)
+                .map(atomicInteger -> BigInteger.valueOf(atomicInteger.longValue()))
+                .orElse(BigInteger.ZERO);
+    }
+
+    public @NotNull BigInteger getOverallStatistic(@NotNull StatisticTypes.OVERALL type) {
+        return this.overallStatisticList
+                .stream()
+                .filter(overallStatistic -> overallStatistic.getType() == type)
+                .findFirst()
+                .map(OverallStatistic::getAmount)
+                .map(atomicInteger -> BigInteger.valueOf(atomicInteger.longValue()))
+                .orElse(BigInteger.ZERO);
+    }
+
+    /*
     Override Methods
      */
     @Override
     public @NotNull CompletableFuture<UserStatisticContainer> withChildren() {
-        return CompletableFuture.completedFuture(this);
+        CompletableFuture<?> futureGame = UserStatisticDAO.readGame(this).thenAccept(this.gameStatisticList::addAll);
+        CompletableFuture<?> futureOverall = UserStatisticDAO.readOverall(this).thenAccept(this.overallStatisticList::addAll);
+        return CompletableFuture.allOf(futureGame, futureOverall).thenApply(v -> this);
     }
 }
