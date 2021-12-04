df11a1f1b5c53967acbe7b7fb3ff2fc241bcf56e
Eduard Wayland <eduproard@gmail.com>
2021-12-04 14:16:46 +0100

    Added user factory and dao registration

--- lobby/src/gg/mooncraft/minecraft/bedwars/lobby/BedWarsPlugin.java
+++ lobby/src/gg/mooncraft/minecraft/bedwars/lobby/BedWarsPlugin.java
@@ -1,5 +1,7 @@
 package gg.mooncraft.minecraft.bedwars.lobby;
 
+import lombok.Getter;
+
 import me.eduardwayland.mooncraft.waylander.database.Credentials;
 import me.eduardwayland.mooncraft.waylander.database.Database;
 import me.eduardwayland.mooncraft.waylander.database.connection.hikari.impl.MariaDBConnectionFactory;
@@ -9,20 +11,32 @@ import org.jetbrains.annotations.NotNull;
 import gg.mooncraft.minecraft.bedwars.common.ComplexJavaPlugin;
 import gg.mooncraft.minecraft.bedwars.common.messaging.RedisChannel;
 import gg.mooncraft.minecraft.bedwars.common.messaging.RedisMessenger;
+import gg.mooncraft.minecraft.bedwars.data.UserDAO;
+import gg.mooncraft.minecraft.bedwars.lobby.factory.UserFactory;
 import gg.mooncraft.minecraft.bedwars.lobby.handlers.commands.Commands;
 import gg.mooncraft.minecraft.bedwars.lobby.handlers.listeners.PlayerListeners;
 import gg.mooncraft.minecraft.bedwars.lobby.messaging.LobbyRedisMessenger;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisPoolConfig;
 
+@Getter
 public class BedWarsPlugin extends ComplexJavaPlugin {
 
+    /*
+    Fields
+     */
+    private UserFactory userFactory;
 
     /*
     Override Methods
      */
     @Override
     public void onEnable() {
+        // Load daos
+        UserDAO.registerDAO(getDatabase());
+
+        // Load factories
+        this.userFactory = new UserFactory();
 
         // Listeners
         new PlayerListeners();
---
+++ lobby/src/gg/mooncraft/minecraft/bedwars/lobby/factory/UserFactory.java
@@ -0,0 +1,38 @@
+package gg.mooncraft.minecraft.bedwars.lobby.factory;
+
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import gg.mooncraft.minecraft.bedwars.data.UserDAO;
+import gg.mooncraft.minecraft.bedwars.data.user.BedWarsUser;
+import gg.mooncraft.minecraft.bedwars.lobby.BedWarsPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public final class UserFactory {
+
+    /*
+    Fields
+     */
+    private final @NotNull List<BedWarsUser> userList = new ArrayList<>();
+
+    /*
+    Methods
+     */
+    public void loadUser(@NotNull Player player) {
+        UserDAO.read(player.getUniqueId()).thenAccept(bedWarsUser -> {
+            this.userList.add(bedWarsUser);
+            BedWarsPlugin.getInstance().getLogger().info(player.getName() + "'s data has been loaded!");
+        });
+    }
+
+    public void unloadUser(@NotNull Player player) {
+        this.userList.removeIf(bedWarsUser -> bedWarsUser.getUniqueId().equals(player.getUniqueId()));
+    }
+
+    public @NotNull Optional<BedWarsUser> getUser(@NotNull Player player) {
+        return this.userList.stream().filter(bedWarsUser -> bedWarsUser.getUniqueId().equals(player.getUniqueId())).findFirst();
+    }
+}
