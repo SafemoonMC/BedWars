d6a7009c0f5a5a6c35e3d7e3bbe4322e6331d2d0
Eduard Wayland <eduproard@gmail.com>
2021-12-09 21:19:32 +0100

    Preparing main instance with mysql, redis and messaging system

--- game/src/gg/mooncraft/minecraft/bedwars/game/BedWarsPlugin.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/BedWarsPlugin.java
@@ -1,5 +1,55 @@
 package gg.mooncraft.minecraft.bedwars.game;
 
-public class BedWarsPlugin {
+import me.eduardwayland.mooncraft.waylander.database.Credentials;
+import me.eduardwayland.mooncraft.waylander.database.Database;
+import me.eduardwayland.mooncraft.waylander.database.connection.hikari.impl.MariaDBConnectionFactory;
 
+import org.jetbrains.annotations.NotNull;
+
+import gg.mooncraft.minecraft.bedwars.common.ComplexJavaPlugin;
+import gg.mooncraft.minecraft.bedwars.common.messaging.RedisChannel;
+import gg.mooncraft.minecraft.bedwars.common.messaging.RedisMessenger;
+import gg.mooncraft.minecraft.bedwars.game.messaging.GameRedisMessenger;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class BedWarsPlugin extends ComplexJavaPlugin {
+
+    /*
+    Override Methods
+     */
+    @Override
+    public void onEnable() {
+
+        getLogger().info("Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        super.shutdown();
+        getLogger().info("Disabled!");
+    }
+
+    @Override
+    public @NotNull Database createDatabase(@NotNull Credentials credentials) {
+        return Database.builder()
+                .identifier(getName())
+                .scheduler(getScheduler())
+                .connectionFactory(new MariaDBConnectionFactory(getName(), credentials))
+                .build();
+    }
+
+    @Override
+    public @NotNull RedisMessenger createRedisMessenger(@NotNull JedisPoolConfig jedisPoolConfig, @NotNull HostAndPort hostAndPort, @NotNull String username, @NotNull String password) {
+        RedisMessenger redisMessenger = new RedisMessenger(this, new GameRedisMessenger(this));
+        redisMessenger.init(RedisChannel.LOBBY, jedisPoolConfig, hostAndPort, username, password);
+        return redisMessenger;
+    }
+
+    /*
+    Static Methods
+     */
+    public static @NotNull BedWarsPlugin getInstance() {
+        return BedWarsPlugin.getPlugin(BedWarsPlugin.class);
+    }
 }
---
+++ game/src/gg/mooncraft/minecraft/bedwars/game/messaging/GameRedisMessenger.java
@@ -0,0 +1,81 @@
+package gg.mooncraft.minecraft.bedwars.game.messaging;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import gg.mooncraft.minecraft.bedwars.common.messaging.consumer.IncomingMessageConsumer;
+import gg.mooncraft.minecraft.bedwars.common.messaging.message.Message;
+import gg.mooncraft.minecraft.bedwars.common.utilities.CaffeineList;
+import gg.mooncraft.minecraft.bedwars.common.utilities.gson.GsonProvider;
+import gg.mooncraft.minecraft.bedwars.game.BedWarsPlugin;
+import gg.mooncraft.minecraft.bedwars.game.MessageType;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BiFunction;
+
+public final class GameRedisMessenger implements IncomingMessageConsumer {
+
+
+    /*
+    Fields
+     */
+    private final @NotNull BedWarsPlugin plugin;
+    private final @NotNull CaffeineList<UUID> receivedMessageList;
+
+    /*
+    Constructor
+     */
+    public GameRedisMessenger(@NotNull BedWarsPlugin plugin) {
+        this.plugin = plugin;
+        this.receivedMessageList = new CaffeineList<>(10, TimeUnit.SECONDS);
+    }
+
+    /*
+    Override Methods
+     */
+    @Override
+    public boolean consumeIncomingMessageAsString(@NotNull String jsonMessage) {
+        JsonObject jsonObject = GsonProvider.normal().fromJson(jsonMessage, JsonObject.class).getAsJsonObject();
+
+        // Get the unique-id of the message if any
+        JsonElement uniqueIdElement = jsonObject.get("unique-id");
+        if (uniqueIdElement == null) {
+            throw new IllegalStateException("The incoming message has no unique-id argument: " + jsonMessage);
+        }
+        UUID uniqueId = UUID.fromString(uniqueIdElement.getAsString());
+
+        // Check if a message with that unique-id has already been received, return if so
+        if (!this.receivedMessageList.add(uniqueId)) return false;
+
+        // Get the type of the message if any
+        JsonElement typeElement = jsonObject.get("type");
+        if (typeElement == null) {
+            throw new IllegalStateException("The incoming message has no type argument: " + jsonMessage);
+        }
+        String type = typeElement.getAsString();
+
+        // Check if the type can be recognised and return false if not
+        @Nullable BiFunction<UUID, JsonElement, Message> messageSupplier = MessageType.getSupplierFor(type);
+        if (messageSupplier == null) return false;
+
+        // Get the final message if any and return false if not
+        JsonElement contentElement = jsonObject.get("content");
+        if (contentElement == null) return false;
+
+        // Process the incoming message
+        Message message = messageSupplier.apply(uniqueId, contentElement);
+        processIncomingMessage(message);
+        return false;
+    }
+
+    /*
+    Methods
+     */
+    private void processIncomingMessage(@NotNull Message message) {
+
+    }
+}
