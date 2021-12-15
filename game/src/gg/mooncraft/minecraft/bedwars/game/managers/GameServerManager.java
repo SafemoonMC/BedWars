a3159767c7bfdbe46b20f5b40b65743d318ae7e1
Eduard Wayland <eduproard@gmail.com>
2021-12-16 00:32:33 +0100

    Preparing GameServer manager

---
+++ game/src/gg/mooncraft/minecraft/bedwars/game/managers/GameServerManager.java
@@ -0,0 +1,11 @@
+package gg.mooncraft.minecraft.bedwars.game.managers;
+
+public final class GameServerManager {
+
+    /*
+    Methods
+     */
+    public void sendGameServerMessage() {
+
+    }
+}
