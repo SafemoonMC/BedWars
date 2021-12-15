40b56d70627d79f45d0ae26f50ef91ea46ba4c51
Eduard Wayland <eduproard@gmail.com>
2021-12-16 00:48:19 +0100

    Added list and map view methods

--- game/src/gg/mooncraft/minecraft/bedwars/game/managers/MapManager.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/managers/MapManager.java
@@ -1,12 +1,14 @@
 package gg.mooncraft.minecraft.bedwars.game.managers;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnmodifiableView;
 
 import gg.mooncraft.minecraft.bedwars.data.MapDAO;
 import gg.mooncraft.minecraft.bedwars.data.map.BedWarsMap;
 import gg.mooncraft.minecraft.bedwars.game.BedWarsPlugin;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +20,7 @@ public final class MapManager {
     Fields
      */
     private final @NotNull List<BedWarsMap> mapList = new ArrayList<>();
-    private final @NotNull Map<String, SlimeBukkitPair> mapWorldsMap = new HashMap<>();
+    private final @NotNull Map<String, SlimeBukkitPair> worldsMap = new HashMap<>();
 
     /*
     Constructor
@@ -50,6 +52,16 @@ public final class MapManager {
 
     public void storeMap(@NotNull String name, @NotNull BedWarsMap bedWarsMap, @NotNull SlimeBukkitPair slimeBukkitPair) {
         this.mapList.add(bedWarsMap);
-        this.mapWorldsMap.put(name, slimeBukkitPair);
+        this.worldsMap.put(name, slimeBukkitPair);
+    }
+
+    @UnmodifiableView
+    public @NotNull List<BedWarsMap> getMapList() {
+        return Collections.unmodifiableList(this.mapList);
+    }
+
+    @UnmodifiableView
+    public @NotNull Map<String, SlimeBukkitPair> getWorldsMap() {
+        return Collections.unmodifiableMap(this.worldsMap);
     }
 }
