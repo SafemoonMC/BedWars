38bceaddd3da224a0a93a6b2e4b2659a9b9c6c68
Eduard Wayland <eduproard@gmail.com>
2021-12-16 01:05:26 +0100

    Added /bw setup <cancel or complete> commands

--- game/src/gg/mooncraft/minecraft/bedwars/game/handlers/commands/Commands.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/handlers/commands/Commands.java
@@ -10,6 +10,8 @@ import me.eduardwayland.mooncraft.waylander.command.builders.LiteralCommandBuild
 import me.eduardwayland.mooncraft.waylander.command.builders.RequiredCommandBuilder;
 
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
 
 import gg.mooncraft.minecraft.bedwars.game.BedWarsPlugin;
 
@@ -18,11 +20,16 @@ public final class Commands {
 
     public static void loadAll() {
         LiteralCommand<?> command = LiteralCommandBuilder
-                .name("bedwars").aliases("bw")
+                .name("bedwars").aliases("bw").permission(new Permission("bedwars.admin", PermissionDefault.OP))
                 .then(LiteralCommandBuilder.name("create")
                         .then(RequiredCommandBuilder
                                 .<Player, String>name("map-name", StringArgumentType.word())
                                 .executes((sender, arguments) -> {
+                                    if (BedWarsPlugin.getInstance().getSetupManager().getMapBuilder(sender).isPresent()) {
+                                        sender.sendMessage("You are already in setup mode. You can cancel the current one with /bw cancel.");
+                                        return;
+                                    }
+
                                     String mapName = arguments.getArgument("map-name", String.class);
                                     if (!BedWarsPlugin.getInstance().getMapManager().getWorldsMap().containsKey(mapName)) {
                                         sender.sendMessage("There is already a map with that name.");
@@ -30,6 +37,28 @@ public final class Commands {
                                     }
                                     BedWarsPlugin.getInstance().getSetupManager().startSetup(sender, mapName);
                                     sender.sendMessage("Setup mode has been started for " + mapName + ".");
+                                })
+                        )
+                )
+                .then(LiteralCommandBuilder.name("setup")
+                        .then(LiteralCommandBuilder
+                                .<Player>name("cancel")
+                                .executes(sender -> {
+                                    BedWarsPlugin.getInstance().getSetupManager().getMapBuilder(sender).ifPresentOrElse(mapBuilder -> {
+                                                mapBuilder.cancel();
+                                                sender.sendMessage("The setup has been cancelled.");
+                                            },
+                                            () -> sender.sendMessage("You are not in a setup mode."));
+                                })
+                        )
+                        .then(LiteralCommandBuilder
+                                .<Player>name("complete")
+                                .executes(sender -> {
+                                    BedWarsPlugin.getInstance().getSetupManager().getMapBuilder(sender).ifPresentOrElse(mapBuilder -> {
+                                                mapBuilder.complete();
+                                                sender.sendMessage("The setup has been completed.");
+                                            },
+                                            () -> sender.sendMessage("You are not in a setup mode."));
                                 })
                         )
                 )
