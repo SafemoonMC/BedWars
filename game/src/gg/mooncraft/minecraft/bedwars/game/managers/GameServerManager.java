955aa83b9a12601dbd9e50c5801401b31a35519d
Eduard Wayland <eduproard@gmail.com>
2021-12-18 16:49:30 +0100

    Updated GameServer manager

--- game/src/gg/mooncraft/minecraft/bedwars/game/managers/GameServerManager.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/managers/GameServerManager.java
@@ -1,11 +1,18 @@
 package gg.mooncraft.minecraft.bedwars.game.managers;
 
+import gg.mooncraft.minecraft.bedwars.common.messages.GameServerMessage;
+import gg.mooncraft.minecraft.bedwars.common.messaging.RedisChannel;
+import gg.mooncraft.minecraft.bedwars.game.BedWarsPlugin;
+
+import java.util.Collections;
+
 public final class GameServerManager {
 
     /*
     Methods
      */
     public void sendGameServerMessage() {
-
+        GameServerMessage gameServerMessage = new GameServerMessage(new GameServerMessage.GameServer(BedWarsPlugin.getInstance().getServerName(), Collections.emptyList()));
+        BedWarsPlugin.getInstance().getMessenger().sendMessage(RedisChannel.LOBBY, gameServerMessage);
     }
 }
