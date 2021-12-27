bc0b25c84a254639fdf414501aaf6d1bff68a195
Eduard Wayland <eduproard@gmail.com>
2021-12-27 22:46:13 +0100

    Added SlimeBukkitPair location getter

--- game/src/gg/mooncraft/minecraft/bedwars/game/slime/SlimeBukkitPair.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/slime/SlimeBukkitPair.java
@@ -2,10 +2,14 @@ package gg.mooncraft.minecraft.bedwars.game.slime;
 
 import com.grinderwolf.swm.api.world.SlimeWorld;
 
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
 
 public record SlimeBukkitPair(@NotNull SlimeWorld slimeWorld, @NotNull World world,
                               boolean persistent) {
 
+    public @NotNull Location getLocation(double x, double y, double z, float yaw, float pitch) {
+        return new Location(world, x, y, z, yaw, pitch);
+    }
 }
