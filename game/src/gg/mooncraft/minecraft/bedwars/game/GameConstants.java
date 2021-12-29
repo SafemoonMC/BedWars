d7ed7275c9e3e1dfe4d44b9f61d64a2432904052
Eduard Wayland <eduproard@gmail.com>
2021-12-29 01:18:38 +0100

    Added scoreboard constants

--- game/src/gg/mooncraft/minecraft/bedwars/game/GameConstants.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/GameConstants.java
@@ -5,11 +5,25 @@ import lombok.NoArgsConstructor;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.List;
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public final class GameConstants {
 
     /*
     Constants
      */
+    public static final @NotNull String DEFAULT_SCOREBOARD_TITLE = "&4~ BedWars ~";
+    public static final @NotNull List<String> DEFAULT_SCOREBOARD_LINES = Arrays.asList(
+            "&7%date% &8%server-name%",
+            "",
+            "&3&l| &r%game-status%",
+            "",
+            "{teams}",
+            "",
+            "&b  play.mooncraft.gg"
+    );
+
     public static final @NotNull String DEFAULT_WORLD_NAME = "VoidworldStart";
 }
