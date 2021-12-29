9462a7cb834791c4dbacc9e6214bed825842a582
Eduard Wayland <eduproard@gmail.com>
2021-12-29 23:30:37 +0100

    Added display field to game states

--- data/src/gg/mooncraft/minecraft/bedwars/data/GameState.java
+++ data/src/gg/mooncraft/minecraft/bedwars/data/GameState.java
@@ -1,6 +1,22 @@
 package gg.mooncraft.minecraft.bedwars.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+@AllArgsConstructor
 public enum GameState {
 
-    LOADING, WAITING, PLAYING, ENDING, UNLOADING
+    LOADING("Loading..."),
+    WAITING("Waiting for players..."),
+    PLAYING("Playing..."),
+    ENDING("Ending..."),
+    UNLOADING("Unloading...");
+
+    /*
+    Fields
+     */
+    private final @NotNull String display;
 }
