53aeca7ed8db30f7b10ed7545ad6d27d709520a6
Eduard Wayland <eduproard@gmail.com>
2021-12-29 22:18:43 +0100

    Added MatchPlayerQuitEvent

---
+++ game/src/gg/mooncraft/minecraft/bedwars/game/events/MatchPlayerQuitEvent.java
@@ -0,0 +1,46 @@
+package gg.mooncraft.minecraft.bedwars.game.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import gg.mooncraft.minecraft.bedwars.game.match.GameMatch;
+import gg.mooncraft.minecraft.bedwars.game.match.GameMatchPlayer;
+import gg.mooncraft.minecraft.bedwars.game.match.GameMatchTeam;
+
+@Getter
+@AllArgsConstructor
+public class MatchPlayerQuitEvent extends Event {
+
+    /*
+    Constants
+     */
+    private static final @NotNull HandlerList HANDLERS = new HandlerList();
+
+    /*
+    Fields
+     */
+    private final @NotNull Player player;
+    private final @NotNull GameMatch gameMatch;
+    private final @NotNull GameMatchTeam gameMatchTeam;
+    private final @NotNull GameMatchPlayer gameMatchPlayer;
+
+    /*
+    Static Methods
+     */
+    public static @NotNull HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    /*
+    Override Methods
+     */
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
+}
--- game/src/gg/mooncraft/minecraft/bedwars/game/handlers/listeners/PlayerListeners.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/handlers/listeners/PlayerListeners.java
@@ -5,11 +5,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.jetbrains.annotations.NotNull;
 
 import gg.mooncraft.minecraft.bedwars.game.BedWarsPlugin;
 import gg.mooncraft.minecraft.bedwars.game.events.EventsAPI;
 import gg.mooncraft.minecraft.bedwars.game.events.MatchPlayerJoinEvent;
+import gg.mooncraft.minecraft.bedwars.game.events.MatchPlayerQuitEvent;
 
 public class PlayerListeners implements Listener {
 
@@ -31,6 +33,17 @@ public class PlayerListeners implements Listener {
                     gameMatch.getTeamOf(player)
                             .ifPresent(gameMatchTeam -> gameMatchTeam.getPlayer(player.getUniqueId())
                                     .ifPresent(gameMatchPlayer -> EventsAPI.callEventSync(new MatchPlayerJoinEvent(player, gameMatch, gameMatchTeam, gameMatchPlayer))));
+                });
+    }
+
+    @EventHandler
+    public void on(@NotNull PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+        BedWarsPlugin.getInstance().getMatchManager().getGameMatch(player)
+                .ifPresent(gameMatch -> {
+                    gameMatch.getTeamOf(player)
+                            .ifPresent(gameMatchTeam -> gameMatchTeam.getPlayer(player.getUniqueId())
+                                    .ifPresent(gameMatchPlayer -> EventsAPI.callEventSync(new MatchPlayerQuitEvent(player, gameMatch, gameMatchTeam, gameMatchPlayer))));
                 });
     }
 }
