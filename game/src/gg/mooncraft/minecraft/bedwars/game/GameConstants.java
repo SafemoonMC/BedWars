f7641a697ec4aaab9cf0372a29cc02a0647cb05b
Eduard Wayland <eduproard@gmail.com>
2021-12-29 18:40:07 +0100

    Fixed empty lines not showing and title

--- game/src/gg/mooncraft/minecraft/bedwars/game/GameConstants.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/GameConstants.java
@@ -14,14 +14,14 @@ public final class GameConstants {
     /*
     Constants
      */
-    public static final @NotNull String DEFAULT_SCOREBOARD_TITLE = "&4~ BedWars ~";
+    public static final @NotNull String DEFAULT_SCOREBOARD_TITLE = "&6&lBedWars";
     public static final @NotNull List<String> DEFAULT_SCOREBOARD_LINES = Arrays.asList(
             "&7%date% &8%server-name%",
-            "",
+            "&r",
             "&3&l| &r%game-status%",
-            "",
+            "&r",
             "{teams}",
-            "",
+            "&r",
             "&b  play.mooncraft.gg"
     );
 
