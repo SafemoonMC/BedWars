22a38ba488299b75317d0a6582a01be3e92aa287
Eduard Wayland <eduproard@gmail.com>
2021-12-29 23:40:25 +0100

    Handling scoreboards and playing right actions on events

--- game/src/gg/mooncraft/minecraft/bedwars/game/handlers/listeners/MatchListeners.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/handlers/listeners/MatchListeners.java
@@ -1,5 +1,6 @@
 package gg.mooncraft.minecraft.bedwars.game.handlers.listeners;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import me.neznamy.tab.api.TabAPI;
 
 import org.bukkit.Bukkit;
@@ -9,11 +10,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.jetbrains.annotations.NotNull;
 
+import gg.mooncraft.minecraft.bedwars.data.GameState;
 import gg.mooncraft.minecraft.bedwars.data.map.point.PointTypes;
 import gg.mooncraft.minecraft.bedwars.game.BedWarsPlugin;
+import gg.mooncraft.minecraft.bedwars.game.GameConstants;
 import gg.mooncraft.minecraft.bedwars.game.events.MatchPlayerJoinEvent;
+import gg.mooncraft.minecraft.bedwars.game.events.MatchPlayerQuitEvent;
+import gg.mooncraft.minecraft.bedwars.game.events.MatchUpdateStateEvent;
 import gg.mooncraft.minecraft.bedwars.game.match.GameMatch;
+import gg.mooncraft.minecraft.bedwars.game.match.GameMatchPlayer;
 import gg.mooncraft.minecraft.bedwars.game.match.GameMatchTeam;
+import gg.mooncraft.minecraft.bedwars.game.match.PlayerStatus;
 
 public class MatchListeners implements Listener {
 
@@ -28,20 +35,69 @@ public class MatchListeners implements Listener {
     Handlers
      */
     @EventHandler
+    public void on(@NotNull MatchUpdateStateEvent e) {
+        GameMatch gameMatch = e.getGameMatch();
+
+        switch (gameMatch.getGameState()) {
+            case PLAYING -> {
+                gameMatch.getScoreboard().unregister();
+
+                for (GameMatchTeam gameMatchTeam : gameMatch.getTeamList()) {
+                    for (GameMatchPlayer gameMatchPlayer : gameMatchTeam.getMatchPlayerList()) {
+                        gameMatchPlayer.getPlayer().ifPresent(player -> {
+                            TabAPI.getInstance().getScoreboardManager().showScoreboard(TabAPI.getInstance().getPlayer(player.getUniqueId()), gameMatchTeam.getScoreboard());
+                        });
+                    }
+                }
+            }
+            case ENDING -> {
+                for (GameMatchTeam gameMatchTeam : gameMatch.getTeamList()) {
+                    gameMatchTeam.getScoreboard().unregister();
+                }
+            }
+        }
+
+        Bukkit.broadcastMessage(gameMatch.getSlimeBukkitPair().getName() + " match status: " + gameMatch.getGameState().name());
+    }
+
+    @EventHandler
     public void on(@NotNull MatchPlayerJoinEvent e) {
         Player player = e.getPlayer();
         GameMatch gameMatch = e.getGameMatch();
         GameMatchTeam gameMatchTeam = e.getGameMatchTeam();
 
+        // Teleport to spawnpoint
         gameMatch.getBedWarsMap()
                 .flatMap(bedWarsMap -> bedWarsMap.getPointsContainer().getGameMapPoint(PointTypes.MAP.MAP_SPAWNPOINT))
                 .ifPresent(gameMapPoint -> {
                     Location location = gameMatch.getSlimeBukkitPair().getLocation(gameMapPoint.getX(), gameMapPoint.getY(), gameMapPoint.getZ(), gameMapPoint.getYaw(), gameMapPoint.getPitch());
-                    BedWarsPlugin.getInstance().getScheduler().executeSync(() -> {
-                        player.teleport(location);
-                        TabAPI.getInstance().getScoreboardManager().showScoreboard(TabAPI.getInstance().getPlayer(player.getUniqueId()), gameMatchTeam.getScoreboard());
-                    });
+                    BedWarsPlugin.getInstance().getScheduler().executeSync(() -> player.teleport(location));
                 });
 
+        // Show scoreboard
+        BedWarsPlugin.getInstance().getScheduler().executeSync(() -> {
+            if (gameMatch.getGameState() == GameState.WAITING) {
+                TabAPI.getInstance().getScoreboardManager().showScoreboard(TabAPI.getInstance().getPlayer(player.getUniqueId()), gameMatch.getScoreboard());
+            } else {
+                TabAPI.getInstance().getScoreboardManager().showScoreboard(TabAPI.getInstance().getPlayer(player.getUniqueId()), gameMatchTeam.getScoreboard());
+            }
+        });
+
+        // Send join message
+        String joinMessage = PlaceholderAPI.setPlaceholders(player, GameConstants.MESSAGE_PLAYER_JOIN);
+        player.sendMessage(joinMessage
+                .replaceAll("%game-players-count%", String.valueOf(gameMatch.getPlayersCount()))
+                .replaceAll("%game-players-capacity%", String.valueOf(gameMatch.getPlayersCapacity()))
+        );
+    }
+
+    @EventHandler
+    public void on(@NotNull MatchPlayerQuitEvent e) {
+        Player player = e.getPlayer();
+        GameMatchPlayer gameMatchPlayer = e.getGameMatchPlayer();
+        gameMatchPlayer.setStatus(PlayerStatus.SPECTATING);
+
+        String quitMessage = PlaceholderAPI.setPlaceholders(player, GameConstants.MESSAGE_PLAYER_QUIT);
+        player.sendMessage(quitMessage);
     }
 }
