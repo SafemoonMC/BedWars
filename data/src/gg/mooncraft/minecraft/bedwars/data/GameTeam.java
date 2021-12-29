4f254edb18070bcd763bf455a432becda1e4f36c
Eduard Wayland <eduproard@gmail.com>
2021-12-29 01:14:42 +0100

    Updating game teams

--- data/src/gg/mooncraft/minecraft/bedwars/data/GameTeam.java
+++ data/src/gg/mooncraft/minecraft/bedwars/data/GameTeam.java
@@ -11,25 +11,27 @@ import org.jetbrains.annotations.NotNull;
 @AllArgsConstructor
 public enum GameTeam {
 
-    BLACK(ChatColor.BLACK),
-    DARK_BLUE(ChatColor.DARK_BLUE),
-    DARK_GREEN(ChatColor.DARK_GREEN),
-    DARK_AQUA(ChatColor.DARK_AQUA),
-    DARK_RED(ChatColor.DARK_RED),
-    DARK_PURPLE(ChatColor.DARK_PURPLE),
-    GOLD(ChatColor.GOLD),
-    GRAY(ChatColor.GRAY),
-    DARK_GRAY(ChatColor.DARK_GRAY),
-    BLUE(ChatColor.BLUE),
-    GREEN(ChatColor.GREEN),
-    AQUA(ChatColor.AQUA),
-    RED(ChatColor.RED),
-    LIGHT_PURPLE(ChatColor.LIGHT_PURPLE),
-    YELLOW(ChatColor.YELLOW),
-    WHITE(ChatColor.WHITE);
+    //    BLACK(ChatColor.BLACK),
+    //    DARK_BLUE(ChatColor.DARK_BLUE),
+    DARK_GREEN('G', "Green", ChatColor.DARK_GREEN),
+    DARK_AQUA('A', "Aqua", ChatColor.DARK_AQUA),
+    DARK_RED('R', "Red", ChatColor.DARK_RED),
+    //    DARK_PURPLE(ChatColor.DARK_PURPLE),
+    //    GOLD(ChatColor.GOLD),
+    //    GRAY(ChatColor.GRAY),
+    DARK_GRAY('S', "Gray", ChatColor.DARK_GRAY),
+    BLUE('B', "Blue", ChatColor.BLUE),
+    //    GREEN(ChatColor.GREEN),
+    //    AQUA(ChatColor.AQUA),
+    //    RED(ChatColor.RED),
+    LIGHT_PURPLE('P', "Pink", ChatColor.LIGHT_PURPLE),
+    YELLOW('Y', "Yellow", ChatColor.YELLOW),
+    WHITE('W', "White", ChatColor.WHITE);
 
     /*
     Fields
      */
+    private final char letter;
+    private final @NotNull String display;
     private final @NotNull ChatColor chatColor;
 }
