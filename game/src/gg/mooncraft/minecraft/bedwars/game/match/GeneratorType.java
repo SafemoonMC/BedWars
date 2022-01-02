bedb47fabb5257bcc7d970f6407d31e7520fd6d7
Eduard Wayland <eduproard@gmail.com>
2022-01-02 21:14:33 +0100

    Added display

--- game/src/gg/mooncraft/minecraft/bedwars/game/match/GeneratorType.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/match/GeneratorType.java
@@ -3,6 +3,7 @@ package gg.mooncraft.minecraft.bedwars.game.match;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,13 +11,14 @@ import org.jetbrains.annotations.NotNull;
 @AllArgsConstructor
 public enum GeneratorType {
 
-    DIAMOND(Material.DIAMOND_BLOCK, Material.DIAMOND),
-    EMERALD(Material.EMERALD_BLOCK, Material.EMERALD);
+    DIAMOND(ChatColor.AQUA + "Diamond", Material.DIAMOND_BLOCK, Material.DIAMOND),
+    EMERALD(ChatColor.DARK_GREEN + "Emerald", Material.EMERALD_BLOCK, Material.EMERALD);
 
 
     /*
     Fields
      */
+    private final @NotNull String display;
     private final @NotNull Material headMaterial;
     private final @NotNull Material dropMaterial;
 
