1a5f79cc147e4b933e08729348c9b24dc544db13
Eduard Wayland <eduproard@gmail.com>
2022-01-05 00:12:42 +0100

    Teleporting player back to island after falling

--- lobby/src/gg/mooncraft/minecraft/bedwars/lobby/handlers/listeners/PlayerListeners.java
+++ lobby/src/gg/mooncraft/minecraft/bedwars/lobby/handlers/listeners/PlayerListeners.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -32,5 +33,16 @@ public class PlayerListeners implements Listener {
     public void on(@NotNull PlayerQuitEvent e) {
         Player player = e.getPlayer();
         BedWarsPlugin.getInstance().getUserFactory().unloadUser(player);
+    }
+
+    @EventHandler
+    public void on(@NotNull PlayerMoveEvent e) {
+        Player player = e.getPlayer();
+        if (e.getFrom().getBlockX() == e.getTo().getBlockX() && e.getFrom().getBlockY() == e.getTo().getBlockY() && e.getFrom().getBlockZ() == e.getTo().getBlockZ()) {
+            return;
+        }
+        if (e.getTo().getBlockY() <= 0) {
+            player.teleport(player.getWorld().getSpawnLocation());
+        }
     }
 }
