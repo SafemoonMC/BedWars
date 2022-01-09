5853870601274e22ad772155b3739279eef8d6c8
Eduard Wayland <eduproard@gmail.com>
2022-01-10 00:22:43 +0100

    Added dynamic colored wool item and inventory checking method

--- game/src/gg/mooncraft/minecraft/bedwars/game/utilities/ItemsUtilities.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/utilities/ItemsUtilities.java
@@ -4,9 +4,12 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+
+import gg.mooncraft.minecraft.bedwars.data.GameTeam;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public final class ItemsUtilities {
@@ -15,5 +18,38 @@ public final class ItemsUtilities {
         ItemStack itemStack = new ItemStack(material);
         itemStack.addItemFlags(ItemFlag.values());
         return itemStack;
+    }
+
+    public static @NotNull ItemStack createWoolitem(@NotNull GameTeam gameTeam) {
+        switch (gameTeam) {
+            case BLUE -> {
+                return createPureItem(Material.BLUE_WOOL);
+            }
+            case YELLOW -> {
+                return createPureItem(Material.YELLOW_WOOL);
+            }
+            case DARK_RED -> {
+                return createPureItem(Material.RED_WOOL);
+            }
+            case DARK_AQUA -> {
+                return createPureItem(Material.CYAN_WOOL);
+            }
+            case DARK_GRAY -> {
+                return createPureItem(Material.GRAY_WOOL);
+            }
+            case DARK_GREEN -> {
+                return createPureItem(Material.GREEN_WOOL);
+            }
+            case LIGHT_PURPLE -> {
+                return createPureItem(Material.PURPLE_WOOL);
+            }
+            default -> {
+                return createPureItem(Material.WHITE_WOOL);
+            }
+        }
+    }
+
+    public static boolean hasEnoughItems(@NotNull Player player, @NotNull Material material, int amount) {
+        return player.getInventory().contains(material, amount);
     }
 }
