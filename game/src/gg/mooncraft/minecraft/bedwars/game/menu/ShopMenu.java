f38920068190c780ca41e1f41db61199fbf4ce11
Eduard Wayland <eduproard@gmail.com>
2022-01-09 11:53:07 +0100

    Fixed 7th category and added design items

--- game/src/gg/mooncraft/minecraft/bedwars/game/menu/ShopMenu.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/menu/ShopMenu.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import net.kyori.adventure.text.Component;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
@@ -13,6 +14,7 @@ import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import gg.mooncraft.minecraft.bedwars.game.GameConstants;
+import gg.mooncraft.minecraft.bedwars.game.items.ItemStackCreator;
 import gg.mooncraft.minecraft.bedwars.game.match.GameMatch;
 import gg.mooncraft.minecraft.bedwars.game.match.GameMatchPlayer;
 import gg.mooncraft.minecraft.bedwars.game.match.GameMatchTeam;
@@ -21,6 +23,7 @@ import gg.mooncraft.minecraft.bedwars.game.shop.ShopCategory;
 import gg.mooncraft.minecraft.bedwars.game.shop.ShopElement;
 import gg.mooncraft.minecraft.bedwars.game.shop.Shops;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -63,18 +66,24 @@ public final class ShopMenu implements ShopInterface {
         this.gameMatch = gameMatch;
         this.inventory = Bukkit.createInventory(this, 54, Component.text(GameConstants.SHOP_ITEMS_TITLE));
         this.categoryMap = new HashMap<>();
+        // Load and select default category
         load();
         select(1);
+        // Place design items which won't be updated anymore
+        Arrays.stream(DESIGN).forEach(slot -> this.inventory.setItem(slot, ItemStackCreator.using(Material.BLACK_STAINED_GLASS_PANE).meta().display("&4").stack().create()));
     }
 
     /*
     Methods
      */
     private void load() {
+        // Clear old references
         this.categoryMap.clear();
+        Arrays.stream(ELEMENTS).forEach(slot -> this.inventory.setItem(slot, null));
 
-        Shop shop = Shops.getShop(gameMatch.getGameMode());
+        // Place categories item
         int index = 0;
+        Shop shop = Shops.getShop(gameMatch.getGameMode());
         for (ShopCategory shopCategory : shop.getCategoryList()) {
             int slot = CATEGORIES[index];
             ItemStack itemStack = shopCategory.getIconItem().clone();
@@ -87,7 +96,7 @@ public final class ShopMenu implements ShopInterface {
     }
 
     private void select(int categorySlot) {
-        if (categorySlot < 0 || categorySlot > 6) return;
+        if (categorySlot < 0 || categorySlot > 7) return;
         if (selectedCategorySlot != -1) {
             load();
         }
@@ -113,5 +122,9 @@ public final class ShopMenu implements ShopInterface {
      */
     @Override
     public void onClick(int slot, @NotNull Player player, @NotNull GameMatchPlayer gameMatchPlayer, @NotNull GameMatchTeam gameMatchTeam) {
+        if (this.categoryMap.containsKey(slot)) {
+            select(slot);
+            return;
+        }
     }
 }
