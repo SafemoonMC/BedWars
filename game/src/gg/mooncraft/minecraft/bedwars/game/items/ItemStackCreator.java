26b9b715612d0325eaef4733f2680e20f7b8a83d
Eduard Wayland <eduproard@gmail.com>
2022-01-10 00:22:10 +0100

    Added enchant method to the builder

--- game/src/gg/mooncraft/minecraft/bedwars/game/items/ItemStackCreator.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/items/ItemStackCreator.java
@@ -18,13 +18,13 @@ import java.util.UUID;
 @Getter(value = AccessLevel.PACKAGE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ItemStackCreator {
-    
+
     /*
     Fields
      */
     @Setter(value = AccessLevel.PACKAGE)
-    private  @NotNull ItemStack itemStack;
-    
+    private @NotNull ItemStack itemStack;
+
     /*
     Methods
      */
@@ -32,12 +32,12 @@ public final class ItemStackCreator {
         itemStack.setType(material);
         return this;
     }
-    
+
     public ItemStackCreator amount(int amount) {
         itemStack.setAmount(amount);
         return this;
     }
-    
+
     public ItemStackCreator glowing(boolean glowing) {
         if (itemStack.getItemFlags().contains(ItemFlag.HIDE_ENCHANTS) && itemStack.getEnchantments().containsKey(Enchantment.DURABILITY) && !glowing) {
             itemStack.removeEnchantment(Enchantment.DURABILITY);
@@ -49,49 +49,58 @@ public final class ItemStackCreator {
         }
         return this;
     }
-    
-    
+
+    public ItemStackCreator enchant(Enchantment enchantment, int level) {
+        itemStack.addUnsafeEnchantment(enchantment, level);
+        return this;
+    }
+
     public ItemMetaCreator meta() {
-        if (itemStack.getItemMeta() == null) throw new IllegalArgumentException("This ItemStack doesn't have an ItemMeta instance!");
+        if (itemStack.getItemMeta() == null)
+            throw new IllegalArgumentException("This ItemStack doesn't have an ItemMeta instance!");
         return ItemMetaCreator.using(this, itemStack.getItemMeta());
     }
-    
+
     public <T extends ItemMetaCreator> T meta(Class<T> metaClass) {
-        if (itemStack.getItemMeta() == null) throw new IllegalArgumentException("This ItemStack doesn't have an ItemMeta instance!");
+        if (itemStack.getItemMeta() == null)
+            throw new IllegalArgumentException("This ItemStack doesn't have an ItemMeta instance!");
         return ItemMetaCreator.using(this, itemStack.getItemMeta(), metaClass);
     }
-    
+
     public ItemStack create() {
         return itemStack;
     }
-    
+
     /*
     Static Methods
      */
     public static ItemStackCreator using(@NotNull ItemStack itemStack) {
         return new ItemStackCreator(itemStack);
     }
-    
+
     public static ItemStackCreator using(@NotNull Material material) {
         return new ItemStackCreator(new ItemStack(material));
     }
-    
+
     public static ItemStackCreator using(@NotNull ConfigurationSection configurationSection) {
         ItemStack itemStack;
         if (configurationSection.contains("material")) {
             Material material = Material.matchMaterial(configurationSection.getString("material"));
-            if (material == null) throw new IllegalArgumentException("There is no material called " + configurationSection.getString("material"));
+            if (material == null)
+                throw new IllegalArgumentException("There is no material called " + configurationSection.getString("material"));
             itemStack = new ItemStack(material);
         } else if (configurationSection.contains("head-texture")) {
             String headTexture = configurationSection.getString("head-texture");
             itemStack = new ItemStack(Material.PLAYER_HEAD);
-            
+
             UUID hashAsUniqueId = new UUID(headTexture.hashCode(), headTexture.hashCode());
             Bukkit.getUnsafe().modifyItemStack(itemStack, "{SkullOwner:{Id:\"" + hashAsUniqueId + "\",Properties:{textures:[{Value:\"" + headTexture + "\"}]}}}");
-        } else throw new IllegalArgumentException("No ItemStack can be created from that ConfigurationSection!");
+        } else
+            throw new IllegalArgumentException("No ItemStack can be created from that ConfigurationSection!");
         ItemStackCreator itemStackCreator = new ItemStackCreator(itemStack).amount(configurationSection.getInt("amount", 1)).glowing(configurationSection.getBoolean("glowing", false));
-        
+
         if (itemStack.getItemMeta() == null) return itemStackCreator;
-        else return ItemMetaCreator.using(itemStackCreator, itemStack.getItemMeta(), configurationSection).stack();
+        else
+            return ItemMetaCreator.using(itemStackCreator, itemStack.getItemMeta(), configurationSection).stack();
     }
 }
