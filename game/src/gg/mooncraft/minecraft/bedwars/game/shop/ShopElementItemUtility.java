086eedd89266fa21664132d3363488c8c24bdec1
Eduard Wayland <eduproard@gmail.com>
2022-01-13 18:23:58 +0100

    Improved utility items

--- game/src/gg/mooncraft/minecraft/bedwars/game/shop/ShopElementItemUtility.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/shop/ShopElementItemUtility.java
@@ -6,9 +6,11 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import gg.mooncraft.minecraft.bedwars.game.items.ItemStackCreator;
 import gg.mooncraft.minecraft.bedwars.game.match.GameMatchPlayer;
 import gg.mooncraft.minecraft.bedwars.game.match.options.OptionEntry;
 
+import java.util.List;
 import java.util.function.Function;
 
 @Getter
@@ -25,5 +27,13 @@ public final class ShopElementItemUtility extends ShopElement {
     public ShopElementItemUtility(@NotNull String display, @NotNull String description, @NotNull OptionEntry<Material, Integer> cost, @NotNull Material displayIcon, @NotNull Function<GameMatchPlayer, ItemStack> itemStackFunction) {
         super(display, description, cost, displayIcon);
         this.itemStackFunction = itemStackFunction;
+    }
+
+    /*
+    Override Methods
+     */
+    public @NotNull ItemStack getItemStack(@NotNull GameMatchPlayer gameMatchPlayer) {
+        ItemStack itemStack = this.itemStackFunction.apply(gameMatchPlayer).clone();
+        return ItemStackCreator.using(itemStack).meta().display(getDisplay()).lore(List.of(getDescription())).stack().create();
     }
 }
