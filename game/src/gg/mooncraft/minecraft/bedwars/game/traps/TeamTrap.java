3f6373a4112c2650bac9594168f3922333bb905a
Eduard Wayland <eduproard@gmail.com>
2022-01-17 16:22:34 +0100

    Updated traps icon material

--- game/src/gg/mooncraft/minecraft/bedwars/game/traps/TeamTrap.java
+++ game/src/gg/mooncraft/minecraft/bedwars/game/traps/TeamTrap.java
@@ -19,15 +19,16 @@ import java.util.Arrays;
 @AllArgsConstructor
 public enum TeamTrap {
 
-    TRAP_1("It's a trap!", "&7Inflicts Blindness and Slowness\n&7for 8 seconds.", new OptionEntry<>(Material.DIAMOND, 1)),
-    TRAP_2("Counter-Offensive Trap", "&7Grants Speed II and Jump Boost\n&7II for 15 seconds to allied\n&7players near your base.", new OptionEntry<>(Material.DIAMOND, 1)),
-    TRAP_3("Alarm Trap", "&7Reveals invisible players as\n&7well as their name and team.", new OptionEntry<>(Material.DIAMOND, 1)),
-    TRAP_4("Miner Fatigue Trap", "&7Inflict Mining Fatigue for 10\n&7seconds.", new OptionEntry<>(Material.DIAMOND, 1));
+    TRAP_1(Material.TRIPWIRE_HOOK, "It's a trap!", "&7Inflicts Blindness and Slowness\n&7for 8 seconds.", new OptionEntry<>(Material.DIAMOND, 1)),
+    TRAP_2(Material.FEATHER, "Counter-Offensive Trap", "&7Grants Speed II and Jump Boost\n&7II for 15 seconds to allied\n&7players near your base.", new OptionEntry<>(Material.DIAMOND, 1)),
+    TRAP_3(Material.REDSTONE_TORCH, "Alarm Trap", "&7Reveals invisible players as\n&7well as their name and team.", new OptionEntry<>(Material.DIAMOND, 1)),
+    TRAP_4(Material.IRON_PICKAXE, "Miner Fatigue Trap", "&7Inflict Mining Fatigue for 10\n&7seconds.", new OptionEntry<>(Material.DIAMOND, 1));
 
 
     /*
     Fields
      */
+    private final @NotNull Material material;
     private final @NotNull String display;
     private final @NotNull String description;
     private final @NotNull OptionEntry<Material, Integer> costEntry;
@@ -38,10 +39,10 @@ public enum TeamTrap {
     public @NotNull ItemStack getIconItem(@NotNull Player player, @NotNull GameMatchPlayer gameMatchPlayer) {
         String color = player.getInventory().contains(this.costEntry.getKey(), this.costEntry.getValue()) ? "&a" : "&c";
         String lastLine = gameMatchPlayer.getParent().getTrapList().contains(this) ? "&6The trap is already in place!" : player.getInventory().contains(this.costEntry.getKey(), this.costEntry.getValue()) ? "&eClick to purchase!" : "&cYou can't afford this.";
-        return ItemStackCreator.using(ItemsUtilities.createPureItem(Material.TRIPWIRE_HOOK))
+        return ItemStackCreator.using(ItemsUtilities.createPureItem(getMaterial()))
                 .meta()
                 .display(color + display)
-                .lore(Arrays.asList(getDescription(), "", "&7Cost: " + getCost(), lastLine))
+                .lore(Arrays.asList(getDescription(), "", "&7Cost: " + getCost(), "", lastLine))
                 .stack().create();
     }
 
