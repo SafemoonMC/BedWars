f871833fc4ec7d0d81485f538acf38cf42699d02
Eduard Wayland <eduproard@gmail.com>
2022-01-24 22:49:02 +0100

    Added prestige colors to levels based

--- data/src/gg/mooncraft/minecraft/bedwars/data/Prestige.java
+++ data/src/gg/mooncraft/minecraft/bedwars/data/Prestige.java
@@ -16,4 +16,31 @@ public final class Prestige {
     private final @NotNull String display;
     private final @NotNull ChatColor[] colors;
 
+    /*
+    Methods
+     */
+    public @NotNull String applyColors(int levels) {
+        String levelText = String.valueOf(levels);
+
+        StringBuilder finalText = new StringBuilder();
+        if (levelText.length() == colors.length) {
+            for (int i = 0; i < levelText.length(); i++) {
+                char c = levelText.charAt(i);
+                finalText.append(colors[i]).append(c);
+            }
+            return finalText.toString();
+        }
+        if (levelText.length() > colors.length) {
+            int first = 0;
+            int last = levelText.length() - 1;
+
+            finalText.append(colors[first]).append(levelText.charAt(first));
+            for (int i = first + 1; i < last; i++) {
+                finalText.append(colors[i]).append(levelText.charAt(i));
+            }
+            finalText.append(colors[last]).append(levelText.charAt(last));
+            return finalText.toString();
+        }
+        return levelText;
+    }
 }
